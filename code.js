820f3189081477b92a103666409705ff25b67ef9
Reuben Frith <reubenfrith007@gmail.com>
2022-05-18 22:26:10 +1000

    psuedo code

---
+++ code.js
@@ -0,0 +1,44 @@
+/*
+ADD
+- create add function
+- addition takes two numbers and returns result
+*/
+
+/*
+SUBTRACT
+- create subtract function
+*/
+
+/*
+MULITPLY
+- create multiply function
+*/
+
+/*
+DIVIDE
+- create divide function
+*/
+
+
+/*
+OPERATE
+- takes initial number, operater and second number (in that order) and returns result
+*/
+
+function operate(firstNumber, secondNumber, operator) {
+    
+    // What user clicks on is the operator function that is called
+    
+    
+}
+
+
+/*
+User clicks first number = store as first number
+Then,
+User clicks operator = store as operator
+Then,
+User clicks second number = store as second number
+Then,
+Perform operation and retiurn result
+*/
