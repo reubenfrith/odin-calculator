266b847bf0742c78b1e38c22dd324923cad267b0
Reuben Frith <reubenfrith007@gmail.com>
2022-05-19 22:27:59 +1000

    created calculator layout

--- code.js
+++ code.js
@@ -3,33 +3,45 @@ ADD
 - create add function
 - addition takes two numbers and returns result
 */
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
 
 /*
 SUBTRACT
 - create subtract function
 */
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber
+}
 
 /*
 MULITPLY
 - create multiply function
 */
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
 
 /*
 DIVIDE
 - create divide function
 */
-
+function divide(firstNumber, secondNumber) {
+    if (secondNumber == 0) {
+        return "ROOKIE"
+    }
+    else {
+        return firstNumber / secondNumber
+    }
+}
 
 /*
 OPERATE
 - takes initial number, operater and second number (in that order) and returns result
 */
-
 function operate(firstNumber, secondNumber, operator) {
-    
-    // What user clicks on is the operator function that is called
-    
-    
+    return operator(firstNumber,secondNumber);
 }
 
 
